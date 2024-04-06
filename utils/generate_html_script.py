0b1677193b5a25af63f9cd3619e2736252073a33
Erin Watson <watson.e.and@gmail.com>
2024-04-06 16:43:19 +0000

    Modified html generator script to use structure of this dataset

--- utils/generate_html_script.py
+++ utils/generate_html_script.py
@@ -1,18 +1,27 @@
 # This script generates the html file for each bone
 
 import re
+import os
 
 from collections import namedtuple
 
-
+# get the names of all model files (in carcas/carcas-models/models)
+model_dir = os.path.join("carcas-models/models")
+bone_filenames = os.listdir(model_dir)
 
 # create a tuple with required variations for each bone
 Bone = namedtuple('Bone', 'bone_name bone_model_path bone_string')
 
-# TODO reformat filepaths to work on all OS's using the os package or pathlib package
-bones = [Bone('alpaca-3rd-carpal-l', '../alpaca_3rd_carpal_L/Alpaca 3rd Carpal L.glb', 'Alpaca 3rd Carpal L'),
-         Bone('alpaca-4th-carpal-l', '../alpaca_4th_carpal_L/Alpaca 4th Carpal L.glb', 'Alpaca 4th Carpal L'),
-         Bone('alpaca-cranium', '../alpaca_cranium/Alpaca Cranium.glb', 'Alpaca Cranium')]
+bones = []
+for bone_filename in bone_filenames:
+    bone_path = os.path.join("../carcas-models/models", bone_filename)
+    bone_string = bone_filename[:-4] # remove .glb
+    bone_name = bone_string.lower().replace(" ", "-")
+
+# # TODO reformat filepaths to work on all OS's using the os package or pathlib package
+# bones = [Bone('alpaca-3rd-carpal-l', '../alpaca_3rd_carpal_L/Alpaca 3rd Carpal L.glb', 'Alpaca 3rd Carpal L'),
+#          Bone('alpaca-4th-carpal-l', '../alpaca_4th_carpal_L/Alpaca 4th Carpal L.glb', 'Alpaca 4th Carpal L'),
+#          Bone('alpaca-cranium', '../alpaca_cranium/Alpaca Cranium.glb', 'Alpaca Cranium')]
 
 # get the template
 with open("template.html") as f:
@@ -28,7 +37,7 @@ for bone in bones:
     template_filled = bone_alt_filled
 
     # save to a new file
-    bone_html_file = open(f"../aaa_generated_files/{bone.bone_name}.html", "a")
+    bone_html_file = open(f"../html-files/{bone.bone_name}.html", "w")
     bone_html_file.write(template_filled)
     bone_html_file.close()
 
